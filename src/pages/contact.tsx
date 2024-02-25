f35a68df9306503d714867e4e1013cd501dedc7c
BiG A <antoine.salameh.45@gmail.com>
2024-02-25 14:52:20 +0200

    fixed the icon issue

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,23 +13,23 @@ const Contact: React.FC = () => {
                 as BiG A or even BiG A Codes, basically wherever you see this logo of mine.
                 </p>
                 <div>
-                    <a href="https://www.linkedin.com/in/antoine-salameh-7705471ab/" target="_blank" rel="noopener noreferrer" className='fa'>
+                    <a href="https://www.linkedin.com/in/antoine-salameh-7705471ab/" target="_blank" rel="noopener noreferrer" className='contact-fa'>
                     <i className="fab fa-linkedin"></i>
                     </a>
 
-                    <a href="mailto: antoine.salameh.45@gmail.com" target="_blank" rel="noopener noreferrer" className='fa'>
+                    <a href="mailto: antoine.salameh.45@gmail.com" target="_blank" rel="noopener noreferrer" className='contact-fa'>
                     <i className="fa-solid fa-square-envelope"></i>
                     </a>
 
-                    <a href="tel:9613913210" target="_blank" rel="noopener noreferrer" className='fa'>
+                    <a href="tel:9613913210" target="_blank" rel="noopener noreferrer" className='contact-fa'>
                     <i className="fa-solid fa-square-phone"></i>
                     </a>
                     
-                    <a href="https://wa.me/9613913210" target="_blank" rel="noopener noreferrer" className='fa'>
+                    <a href="https://wa.me/9613913210" target="_blank" rel="noopener noreferrer" className='contact-fa'>
                     <i className="fab fa-square-whatsapp"></i>
                     </a>
 
-                    <a href="https://github.com/AntoineSalameh45" target="_blank" rel="noopener noreferrer" className='fa'>
+                    <a href="https://github.com/AntoineSalameh45" target="_blank" rel="noopener noreferrer" className='contact-fa'>
                     <i className="fab fa-square-github"></i>
                     </a>
                 </div>
